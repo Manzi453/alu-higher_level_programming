c620200f4f7b73d145e841b72b56efa1923240c6
Manzi453 <i.manziyamu@alustudent.com>
2024-10-30 14:57:16 -0700

    3-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+"""Define asquare"""
+
+class Square:
+    """Creates a Square.
+    Private instance attributes: size
+    """
+
+    def __init__(self, size=0)
+    """initializes data""" 
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+        self.__size = size
---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """"Creates a square.
+    private instance attrributes: size 
+    """
+    def __init__(self, size=0):
+        """initializes the data"""
+        if not isinstance(size,)
+        raise TypeError("size must be an integer"
+                elif size < 0:
+                raise ValueError("size V I must be >= 0")
+                self.__size = size
+                
+                def area(self)
+                """Return square area"""
+                return self.__size ** 2
