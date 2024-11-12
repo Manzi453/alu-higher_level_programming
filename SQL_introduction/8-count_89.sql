92a2c21f57c9ef28f8ff6dcb2dec82a3ffbf196d
Manzi453 <i.manziyamu@alustudent.com>
2024-11-12 03:26:31 -0800

    last push

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,14 @@
---- count 
-SELECT COUNT(*) FROM first_table WHERE id=89;
+#!/usr/bin/env bash
+
+# Check if the database name is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <database_name>"
+  exit 1
+fi
+
+# Assign the first argument to the DATABASE variable
+DATABASE="$1"
+
+# Run the MySQL command to count the records with id = 89
+mysql -u root -p -D "$DATABASE" -se "SELECT COUNT(*) FROM first_table WHERE id = 89;"
+
